5db9b36bae7f7b1a31f6c8f3c17acf5225be845a
solanki <anujaj2222.gmail.com>
2022-03-10 14:30:36 +0530

    added:by using case statement

--- empwage.sh
+++ empwage.sh
@@ -3,15 +3,18 @@
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+empCheck=$((RANDOM%3));
 
-if [ $isFullTime -eq $randomCheck ];
-then
-	empHrs=8;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=4;
-else
-	empHrs=0;
-fi
-	salary=$(($empHrs*$empRatePerHr));
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+		;;
+	$isPartTime)
+		empHrs=4
+		;;
+	*)
+	empHrs=0
+		;;
+esac
+
+salary=$(($empHrs*$empRatePerHr));
