6672296424d99742e21013aa2238064c4c639348
solanki <anujaj2222.gmail.com>
2022-03-10 14:16:31 +0530

    added:adding part time

--- empwage.sh
+++ empwage.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 
-ispresent=1;
-randomCheck=$((RANDOM%2));
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
 
-if [ $ispresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ];
 then
-	empRatePerHr=20;
 	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr));
+elif [ $isPartTime -eq $randomCheck ];
+then
+	empHrs=4;
 else
-	salary=0;
+	empHrs=0;
 fi
+	salary=$(($empHrs*$empRatePerHr));
